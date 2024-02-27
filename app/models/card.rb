7e72d0c0a3334134fc0e413855f55f92132ebf69
Juan Carlos Medina <jmedina12@cmc.edu>
2024-02-27 11:10:10 -0800

    save tiles on a new card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :lobby
   has_many :card_tiles, dependent: :destroy
+  accepts_nested_attributes_for :card_tiles
   has_many :tiles, through: :card_tiles
 end
