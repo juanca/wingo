930b804a914bb2d140b449173ca130b97ebfc54f
Juan Carlos Medina <jmedina12@cmc.edu>
2024-02-29 13:56:28 -0800

    ensure lobby titles are present

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -2,6 +2,8 @@ class Lobby < ApplicationRecord
   has_many :cards, dependent: :destroy
   has_many :tiles, dependent: :destroy
 
+  validates_presence_of :title
+
   after_create do
     Tile.new(lobby_id: self.id, body: "FREE").save!
   end
